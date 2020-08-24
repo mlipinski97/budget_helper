006e06f62ceae1b3969ca3843286c7477690d7ed
Maciej <maciekl3131@gmail.com>
2020-08-24 15:21:53 +0200

    -added icons for settings activity

--- app/src/main/java/com/example/engineerdegreeapp/activity/SettingsActivity.java
+++ app/src/main/java/com/example/engineerdegreeapp/activity/SettingsActivity.java
@@ -1,5 +1,7 @@
 package com.example.engineerdegreeapp.activity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Bundle;
 
@@ -7,13 +9,18 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+import androidx.preference.SwitchPreferenceCompat;
 
 import com.example.engineerdegreeapp.R;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .registerOnSharedPreferenceChangeListener(this);
         if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
             setTheme(R.style.AppTheme_Dark);
         } else {
@@ -38,10 +45,33 @@ public class SettingsActivity extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals(getString(R.string.dark_mode_preference_key))) {
+            boolean isOn = sharedPreferences.getBoolean(getString(R.string.dark_mode_preference_key), true);
+            SharedPreferences.Editor editor = getSharedPreferences("Settings", MODE_PRIVATE).edit();
+            editor.putBoolean("dark_mode_state", isOn);
+            editor.apply();
+        }
+    }
+
     public static class SettingsFragment extends PreferenceFragmentCompat {
+
+        static SwitchPreferenceCompat switchPreferenceCompat;
+
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.preferences, rootKey);
+            switchPreferenceCompat = (SwitchPreferenceCompat) findPreference("dark_mode");
+            SharedPreferences preferences = getActivity().getSharedPreferences("Settings", Context.MODE_PRIVATE);
+            boolean isOn = preferences.getBoolean("dark_mode_state", true);
+            System.out.println(isOn);
+            if (isOn) {
+                switchPreferenceCompat.setIcon(R.drawable.half_moon_icon);
+            } else {
+                switchPreferenceCompat.setIcon(R.drawable.sun_icon);
+
+            }
         }
     }
 
@@ -54,5 +84,12 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onApplyThemeResource(Resources.Theme theme, int resid, boolean first) {
         super.onApplyThemeResource(theme, resid, first);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .unregisterOnSharedPreferenceChangeListener(this);
     }
 }
